17413d061bb654048fdde80bf2a8a141d6507edb
Pablo Álvarez <pablo.alvarez@lanek.cl>
2022-04-19 17:10:23 -0400

    adfafd2

---
+++ main.py
@@ -0,0 +1,13 @@
+import matplotlib
+import streamlit as st
+import pandas as pd 
+import numpy as np
+
+from scipy.io import wavfile
+import scipy.io as sio
+
+#FFT
+from scipy.fft import fft, fftfreq
+
+import matplotlib.pyplot as plt
+
